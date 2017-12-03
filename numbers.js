8a2e4145e7b887b1edd9f527af2a0a7498f191b5
merp101 <34143165+merp101@users.noreply.github.com>
2017-12-03 18:13:02 -0500

    Update numbers.js

--- numbers.js
+++ numbers.js
@@ -15,16 +15,7 @@ var displayLayerNum = currentLayers;
 
 
 //   NUMBERS
-
-// the "press to double your layers"
-if (mousePressed()) { 
-  if currentLayers > 1 {
-  currentLayers = currentLayers * 2 - 1;
-  }
-  else {
-  currentLayers = currentLayers * 2;
-  }
-};
+  
 // The variable to display (scientific notation)
 if var currentLayers >= 1000 {
   var displayLayerNum = Math.log10(currentLayers);
