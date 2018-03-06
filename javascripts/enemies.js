0967f697e84ee509ccd6811cad5d969268bb7a04
merp101 <34143165+merp101@users.noreply.github.com>
2018-03-05 21:33:18 -0500

    Create enemies.js

---
+++ javascripts/enemies.js
@@ -0,0 +1,19 @@
+var enemies = {
+  cave: {
+    hp: 5,
+    dmg: 1
+  },
+  plains: {
+    hp: 20,
+    dmg: 3
+  },
+  desert: {
+    hp: 50,
+    dmg: 8
+  },
+  dungeon: {
+    hp: 200,
+    dmg: 15
+  }
+}
+export enemies;
