76b92edd2a5ecabc1727624a7eaa6e1a352ad99d
merp101 <34143165+merp101@users.noreply.github.com>
2018-03-12 15:58:55 -0400

    Create cave.js

---
+++ javascripts/cave.js
@@ -0,0 +1,12 @@
+var cave = {
+  level:["  -----------------     ",
+		 			 " /                 \____",
+		 		 	 "|                       ",
+		 			 "|                       ",
+		 			 "|                       ",
+		 			 "| o           =         "],
+  playable:[]
+  init: function() {
+    level[level.length-1].replace("=","*"));
+  }
+}
