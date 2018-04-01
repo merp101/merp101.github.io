28dabe9e6f7ed380f9ea267af2627a6687acc8a7
merp101 <34143165+merp101@users.noreply.github.com>
2018-04-01 19:53:28 -0400

    Update enemies.js

--- javascripts/enemies.js
+++ javascripts/enemies.js
@@ -1,19 +1,12 @@
 var enemies = {
-  cave: {
-    hp: 5,
-    dmg: 1
+  
+  stats: {},
+  
+  init : function () {
+    for (let i = 0;i < 10;i++) {
+      let atk = Math.round(Math.pow(game.zoneNum, Math.pow(1.25, (Math.random() / 10) * 2)));
+      let hp = Math.round(Math.pow(game.zoneNum, Math.pow(1.5, (Math.random() / 10) * 2)));
+      this.stats["n"+SPELLED_NUM[i+1]] = [atk, hp, i];
+    }
   },
-  plains: {
-    hp: 20,
-    dmg: 3
-  },
-  desert: {
-    hp: 50,
-    dmg: 8
-  },
-  dungeon: {
-    hp: 200,
-    dmg: 15
-  }
 }
-
