bf215c99eb36381a8d24b1a19532331083f14dcf
merp101 <34143165+merp101@users.noreply.github.com>
2018-04-11 08:03:25 -0400

    move enemies into game

--- javascripts/config.js
+++ javascripts/config.js
@@ -1,16 +1,33 @@
+const SPELLED_NUM = ["one","two","three","four","five","six","seven"]
 var game = {
-  var player = {
-	money: 0,
-	weapon: new Weapon("fists",1,2,0),//name, attack, speed, cost
-	inventory: [],
+  player = {
+		money: 0,
+		weapon: {
+			attack:1,
+			speed:1,
+			name:"fists"
+		},
+		inventory: [],
+	},
 	zone: {
 		num: 1,
 		max: -1
 	},
+	enemies: {
+		stats: {},
+  	init: function () {
+    	for (let i = 0;i < 10;i++) {
+      	let atk = Math.round(Math.pow(game.zone.num, 1.2));
+      	let hp = Math.round(Math.pow(game.zone.num, 1.2));
+      	this.stats["n"+(i+1)] = [atk, hp, i+1];
+    	}
+  	}
+	},
 	
 	init: function() {
-		enemies.init();
+		this.enemies.init();
 		if (this.zone.max < 1) this.zone.max = 1;
 		if (this.money < 0) this.money = 0;
 	}
+	
 }
