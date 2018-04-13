0422b2a31cc8643ba7d7b5e6d2a95278b8777c97
merp101 <34143165+merp101@users.noreply.github.com>
2018-04-13 17:07:35 -0400

    Update and rename config.js to objects.js

---
+++ javascripts/objects.js
@@ -0,0 +1,48 @@
+const SPELLED_NUM = ["one","two","three","four","five","six","seven"]
+var game = {
+  player = {
+		money: 0,
+		combat: {
+			hp: 10,
+			weapon: {
+				attack:1,
+				speed:1,
+				name:"fists",
+			},
+			fighting: false,
+		},
+		inventory: [],
+	},
+	zone: {
+		num: 1,
+		max: -1,
+	},
+	enemies: {
+		stats: {},
+  	init: function () {
+    	for (let i = 0;i < 10;i++) {
+      	let atk = Math.round(Math.pow(game.zone.num, 1.2)) + 1;
+      	let hp = Math.round(Math.pow(game.zone.num, 1.5)) + 3;
+      	this.stats["n"+(i+1)] = [atk, hp, i+1];
+    	}
+  	}
+	},
+	
+	init: function() {
+		this.enemies.init();
+		if (this.zone.max < 1) this.zone.max = 1;
+		if (this.money < 0) this.money = 0;
+		window.setInterval(gameTick(), 10);
+	}
+	
+}
+
+var quest = {
+	enemyNum: 1,
+	startFight: function() {
+	
+	},
+	fight: function(enemyHP,enemyATK,enemySPD) {
+		if (enemyHP < 1) this.enemyNum ++; return;
+	}
+}
