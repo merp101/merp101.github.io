09595f327bbe4ce76abdf37de130e908ca05be57
merp101 <34143165+merp101@users.noreply.github.com>
2018-05-26 15:57:38 -0400

    Update and rename objects.js to config.js

---
+++ javascripts/config.js
@@ -0,0 +1,34 @@
+var game = {
+	stats: {
+		atk: 0,
+		def: 0,
+		spd: 0,
+		tact: 0,
+		range: 0,
+		magic: 0
+	},
+	items: {
+		equips: {
+			head: "",
+			chest: "",
+			legs: "",
+			boots: "",
+			weapon: "",
+			accessory: ""
+		},
+		consumable: [""],
+		special: [""]
+	},
+	enemies: {
+		num: 0,
+		atk: 0,
+		def: 0,
+		spd: 0,
+		tact: 0,
+		range: 0,
+		magic: 0
+	},
+	misc: {
+		
+	}
+}
