649573430f44e858a0e45fad177cc93c49805530
merp101 <34143165+merp101@users.noreply.github.com>
2018-05-27 15:28:22 -0400

    Update config.js

--- javascripts/config.js
+++ javascripts/config.js
@@ -1,5 +1,10 @@
 var game = {
 	stats: {
+		level: 0,
+		hp: {
+			max: 0,
+			current: 0,
+		},
 		atk: 0,
 		def: 0,
 		spd: 0,
@@ -9,21 +14,23 @@ var game = {
 	},
 	items: {
 		equips: {
-			head: "",
-			chest: "",
-			legs: "",
-			boots: "",
-			weapon: {
-				name: "",
-				type: "",
-			},
-			accessory: ""
+			head: {name: "none", def: 0},
+			chest: {name: "none", def: 0},
+			legs: {name: "none", def: 0},
+			boots: {name: "none", def: 0},
+			weapon: {name: "none", type: "", atk: 0},
+			accessory: {name: "none", atk: 0, def: 0}
 		},
 		consumable: [""],
 		special: [""]
 	},
 	enemies: {
 		num: 0,
+		level: 0,
+		hp: {
+			max: 0,
+			current: 0			
+		},
 		atk: 0,
 		def: 0,
 		spd: 0,
