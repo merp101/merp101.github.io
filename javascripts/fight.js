cd17c6e279f671c306011d8e0c0bd5542a9e7ab9
merp101 <34143165+merp101@users.noreply.github.com>
2018-09-14 11:29:21 -0400

    Update fight.js

--- javascripts/fight.js
+++ javascripts/fight.js
@@ -17,12 +17,12 @@ function setEnemies(difficulty) {
 	game.enemies.spd = Math.round(Math.pow(difficulty, 1.05)) + 1;
 	
 	for (let i = 0; i < game.enemies.num; i++) {
-		enemies[SPELLED[i]+"Enemy"].prototype.level = game.enemies.level;
-		enemies[SPELLED[i]+"Enemy"].prototype.hp.max = game.enemies.hp.max;
-		enemies[SPELLED[i]+"Enemy"].prototype.hp.current = game.enemies.hp.current;
-		enemies[SPELLED[i]+"Enemy"].prototype.atk = game.enemies.atk;
-		enemies[SPELLED[i]+"Enemy"].prototype.def = game.enemies.def;
-		enemies[SPELLED[i]+"Enemy"].prototype.spd = game.enemies.spd;
+		enemies.prototype[SPELLED[i]+"Enemy"].prototype.level = game.enemies.level;
+		enemies.prototype[SPELLED[i]+"Enemy"].prototype.hp.max = game.enemies.hp.max;
+		enemies.prototype[SPELLED[i]+"Enemy"].prototype.hp.current = game.enemies.hp.current;
+		enemies.prototype[SPELLED[i]+"Enemy"].prototype.atk = game.enemies.atk;
+		enemies.prototype[SPELLED[i]+"Enemy"].prototype.def = game.enemies.def;
+		enemies.prototype[SPELLED[i]+"Enemy"].prototype.spd = game.enemies.spd;
 	}
 }
 function startFight(difficulty=1) {
