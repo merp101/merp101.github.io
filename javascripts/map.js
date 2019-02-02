b1e8a939ab1262c3f2bb646453ae949d4592679e
merp101 <34143165+merp101@users.noreply.github.com>
2019-02-02 11:11:25 -0500

    Update map.js

--- javascripts/map.js
+++ javascripts/map.js
@@ -2,11 +2,11 @@ function loadMap(r,c) { //rows, columns
   for (r; r--; r>0) {
     let row = document.createElement("ul");
     row.style = "";
-    map.appendChild("row");
+    document.getElementById("map").appendChild("row");
     for (c; c--; c>0) {
       let column = document.createElement("li");
       column.style = "width: 10%; padding-top: 0.526316vh; padding-bottom: 0.526316vh; font-size: 1.71429vh; border: 5px solid black; text-shadow: rgb(251, 117, 63) 0px 0px 15px;";
-      map.appendChild("column");
+      document.getElementById("map").appendChild("column");
     }
   }
 }
