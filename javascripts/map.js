49ab026bd9725992ed099cd7974bf32c6f38bc99
merp101 <34143165+merp101@users.noreply.github.com>
2019-02-02 10:24:18 -0500

    Create map.js

---
+++ javascripts/map.js
@@ -0,0 +1,11 @@
+function loadMap(r,c) { //rows, columns
+  for (r; r--; r>0) {
+    let row = document.createElement("ul");
+    row.style = "";
+    map.appendChild("row");
+    for (c; c--; c>0) {
+      let column = document.createElement("li");
+      column.style = "width: 10%; padding-top: 0.526316vh; padding-bottom: 0.526316vh; font-size: 1.71429vh; text-shadow: rgb(251, 117, 63) 0px 0px 15px;";
+      map.appendChild("column");
+    }
+  }
