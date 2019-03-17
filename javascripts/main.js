5cdfedbbae3de6dd1ea7b79c4f927ded2bc52952
merp101 <34143165+merp101@users.noreply.github.com>
2019-03-17 08:44:18 -0400

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -85,7 +85,7 @@ function tick(letter=0) {
 	function move() {
 		switch(letter) {
 			case "a": if (char.pos.x > 0) char.pos.x -= 1; break;
-			case "d": if (char.pos.x < element("world-"+(char.pos.y+1)).innerHTML.length - 2) char.pos.x += 1; break;
+			case "d": if (char.pos.x < element("world-"+(char.pos.y+1)).innerHTML.length - 1) char.pos.x += 1; break;
 		}
 		drawPlayer();
 	}
