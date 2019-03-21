73c7d08a2bef8c847c39f305dc2e9e6d5220905f
merp101 <34143165+merp101@users.noreply.github.com>
2019-03-21 09:01:53 -0400

    Create art.js

---
+++ javascripts/art.js
@@ -0,0 +1,31 @@
+/*function loadMap(r,c) { //rows, columns
+  var map = document.getElementById("map");
+  for (r; r--; r>0) {
+    let row = document.createElement("ul");
+    row.style = "";
+    map.appendChild("row");
+    for (c; c--; c>0) {
+      let column = document.createElement("li");
+      column.style = "width: 10%; padding-top: 0.526316vh; padding-bottom: 0.526316vh; font-size: 1.71429vh; border: 5px solid black; text-shadow: rgb(251, 117, 63) 0px 0px 15px;";
+      map.appendChild("column");
+    }
+  }
+}*/
+
+var art = {
+  player: "o",
+  enemy: "x",
+}
+
+var maps = { // (length x height) chars
+  cave: [" ________________ ", // 18x5
+         "/                \\", // the backslash is an escape character, it'll only output one
+         "|                 ", // 
+         "|                 ", // 
+         "|_________________"], //
+  caveEnemyPos: ["5,1","8,1"], //x,y
+  caveDiff: 0,
+  forest: [],
+  desert: [],
+}
+         
