9e79d9973ff69653d2acf01b4d1d318ab889b0f7
merp101 <34143165+merp101@users.noreply.github.com>
2019-03-26 17:36:52 -0400

    Update loot.js

--- javascripts/loot.js
+++ javascripts/loot.js
@@ -1,26 +1,41 @@
 var goldGained = 0;
 var itemsGained = [];
 var message = "";
+var combo = []; //combination of name and chance
+var thresholds = [];
 function getTotalLoot() { 
   
 }
 function getEnemyLoot() {
+  //gold
   let rarity = currentEnemy.rarity;
   goldGained += loot[currentLevel].gold * Math.round(Math.pow(rarity,1.25));
-  let random = Math.random();
+  
+  //items
   var itemChances = [];
-  var items = [];
-  for (i = 0; i < loot[currentLevel.items.length; i++) {
-    items.push(loot[currentLevel].items[i].name);
-    itemChances.push(loot[currentLevel].items[i].chance);
+  for (i = 0; i < loot[currentLevel].items.length; i++) {
+    combo.push([loot[currentLevel].items[i].name,Math.floor(Math.pow(loot[currentLevel].items[i].chance,Math.floor(Math.pow(rarity,0.2))))]);
+  } //this formula is floor(chance^(floor(rarity^.2)))
+  for (i = 0; i < combo.length; i++) {
+    itemChances.push(combo[i][1]);
   }
   itemChances = bubbleSort(itemChances);
+  i = 0;
+  while (i < itemChances.length) {
+    for (let j = 0; j < combo.length; j++) {
+      if (combo[j][1] == itemChances[i]) {
+        thresholds.push(combo[j]);
+        i++;
+        break;
+      }
+    }
+  }
+  let random = Math.random();
   for (i = 0; i < loot[currentLevel].items.length; i++) {
-    if (random <= itemChances[i]) {}
+    if (random <= thresholds[i][1]) {
+      itemsGained.push[thresholds[i][0];
+    }
   }
   if (goldGained == 0 && itemsGained = []) {
   }
 }
-
-
-//how to assign item chance to the drop, so that when it bubbleSort()s it, it gives the right item?
