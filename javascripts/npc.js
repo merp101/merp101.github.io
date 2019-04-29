4815a759402a91ddc01f90c364031533c29d7e03
merp101 <34143165+merp101@users.noreply.github.com>
2019-04-29 07:28:33 -0400

    Update npc.js

--- javascripts/npc.js
+++ javascripts/npc.js
@@ -1,1 +1,12 @@
-
+var npcs = {
+  nurse: {
+    dialogue: [],
+    dialoguecycle: 0,
+    shop: [{name:"",cost:1}]
+  },
+  blacksmith: {
+    dialogue: [],
+    dialoguecycle: 0,
+    shop: [{name:"", cost:1}]
+  }
+}
