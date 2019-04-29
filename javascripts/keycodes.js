02e45d52411b81df629f6821ec27d7dc04eded52
merp101 <34143165+merp101@users.noreply.github.com>
2019-04-28 20:53:05 -0400

    Create keycodes.js

---
+++ javascripts/keycodes.js
@@ -0,0 +1,40 @@
+/*A list of all key codes, upper and lower case
+
+This is in keyboard order, from left to right
+(gotten by doing `console.log(event.keyCode);`)
+
+`: , ~:
+1: , !:
+2: , @:
+3: , #:
+4: , $:
+5: , %:
+6: , ^:
+7: , &:
+8: , *:
+9: , (:
+0: , ):
+-: , _:
+=: , +: 
+
+
+q: 113, Q: 81
+w: 119, W: 87
+e: 101, E: 69
+r: 114, R: 82
+t: 116, T: 84
+y: 121, Y: 89
+u: 117, U: 85
+i: 105, I: 73
+o: 111, O: 79
+p: 112, P: 80
+[: 91, {: 123
+]: 93, }: 125
+\: 92, |: 124
+
+
+
+
+b: 98, B: 66
+
+*/
