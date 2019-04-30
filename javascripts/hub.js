cf2f7fc2dc9739e399bdc0d9bea9fdc8c7bff6b0
merp101 <34143165+merp101@users.noreply.github.com>
2019-04-29 20:13:37 -0400

    Update hub.js

--- javascripts/hub.js
+++ javascripts/hub.js
@@ -1,13 +1,13 @@
 var npcList = ["nurse","blacksmith"];
 var npcs = {
   nurse: {
-    pos: 23,
+    pos: 19,
     dialogue: ["i","a","m"],
     dialoguecycle: 0,
     shop: [{name:"",cost:1}]
   },
   blacksmith: {
-    pos: 47,
+    pos: 42,
     dialogue: ["d","e","a","d"],
     dialoguecycle: 0,
     shop: [{name:"", cost:1}]
