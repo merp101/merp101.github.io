397a9aedc7933d007e325a7fda7db557b42a4901
merp101 <34143165+merp101@users.noreply.github.com>
2019-04-29 20:11:55 -0400

    Update art.js

--- javascripts/art.js
+++ javascripts/art.js
@@ -32,7 +32,7 @@ const maps = { // (length x height) chars. USE <u> TAGS INSTEAD OF UNDERLINES ON
         "|      /bbbbb\\       |  +++  \\         \\   |  --0  \\         \\ ",
         "|     |bbbbbbb|      |  O       __     |   |   O        __    |",
         "|     |bbbbbbb|      | \\|/     |  |    |   |  \\|/      |  |   |",
-        "|                    | / \\     |° |    |   |  / \\      |° |   |"],
+        "<u>|                    | / \\     |° |    |   |  / \\      |° |   |</u>"],
   cave: [" ________________ ", // 18x5
          "/                \\", // the backslash is an escape character, it'll only output one
          "|                 ", // 
