1c4548cc3214bfab6b59a6c5e1eec74476c5052a
merp101 <34143165+merp101@users.noreply.github.com>
2019-04-29 20:09:22 -0400

    Update hub.js

--- javascripts/hub.js
+++ javascripts/hub.js
@@ -2,19 +2,20 @@ var npcList = ["nurse","blacksmith"];
 var npcs = {
   nurse: {
     pos: 
-    dialogue: [],
+    dialogue: ["i","a","m"],
     dialoguecycle: 0,
     shop: [{name:"",cost:1}]
   },
   blacksmith: {
-    dialogue: [],
+    dialogue: ["d","e","a","d"],
     dialoguecycle: 0,
     shop: [{name:"", cost:1}]
   }
 }
 
-function cycleNPCDialogue() {
-  
+function cycleNPCDialogue(npc) {
+  changeText("npcdialogue",npc.dialogue[npc.dialoguecycle]);
+  npc.dialoguecycle++;
 }
   
 
