b4d9650a192f6209223e95751c6e7bcf68eb9197
merp101 <34143165+merp101@users.noreply.github.com>
2019-04-30 20:11:30 -0400

    Update combat.js

--- javascripts/combat.js
+++ javascripts/combat.js
@@ -169,7 +169,7 @@ function drawWorld(level) {
 					}
 				}
 				for (i = 0; i < str.length; i++) {
-					str = str.slice(0,a[i]) + "//" + str.slice(array[i]);
+					str = str.slice(0,a[i]) + "//" + str.slice(a[i]);
 					a.forEach(function(num,index,arr) {arr[index] = num + 1;});			  
 				}
 				if (y = 1) {
