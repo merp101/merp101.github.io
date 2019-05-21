96aad379876f24e8c7495a6cf03101f867806925
merp101 <34143165+merp101@users.noreply.github.com>
2019-05-21 09:23:14 -0400

    Update loot.js

--- javascripts/loot.js
+++ javascripts/loot.js
@@ -24,11 +24,17 @@ function getTotalLoot() { //on mission complete, gain all the stuff
 	let type;
 	for (i = 0; i < itemsGained.length; i++) {
 		type = itemsGained[i].type;
+		if (!game.inventory.gear[type].includes(itemsGained[i])) { 
+			game.inventory.gear[type].push(itemsGained[i]);
+		} else {
+			game.inventory.gear[itemsGained[i].name].level += itemsGained[i].level;
+		}
+		/*
 		switch (type) {
 			case "normal": game.inventory.normal.push(itemsGained[i]); break;
 			case "special": game.inventory.special.push(itemsGained[i]); break;
 			case "consumable": game.inventory.consumable.push(itemsGained[i]); break;
-		}
+		}*/
 	}
 	game.gold += goldGained;
 	
