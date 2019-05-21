3505c44e0d76aa0847c97f70e7fd8920afccda51
merp101 <34143165+merp101@users.noreply.github.com>
2019-05-21 13:46:06 -0400

    Update inventory.js

--- javascripts/inventory.js
+++ javascripts/inventory.js
@@ -1,17 +1,23 @@
 function updateGear() {
 	let gearTypes = ["weapon", "helmet", "chestplate", "gloves", "leggings"];
-	//new gear
 	let elementValue;
 	let gear;
 	for (j = 0; j < gearTypes.length; j++) {
 		elementValue = element(gearTypes[j]).childNodes;
-		gear = game.inventory.gear[gearTypes[j]]
+		gear = game.inventory.gear[gearTypes[j]];
+		//new gear
 		if (gear.length > elementValue.length) {
 			for (i = gear.length - (gear.length - elementValue.length); i < gear.length; i++) {
-				createTextElement(gear[i], "gear", gear[i], 'OPTION', false, false);
+				createTextElement(gear[i] + ", level " + gear[i].level, "gear", gear[i], 'OPTION', false, false);
 				element(gear[i]).value = gear[i];
 			}
 		}
+		// update levels
+		for (i = 0; i < elementValue.length; i++) {
+			elementValue[i].innerHTML = "gear[i] + ", level " + gear[i].level";
+		}
 	}
+	
+	
 
 }
